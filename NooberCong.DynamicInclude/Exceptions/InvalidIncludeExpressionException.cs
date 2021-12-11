bfece8cf5bd84c5cb27e3b5268b65e4714987c60
Undying-Hacker <55393499+Undying-Hacker@users.noreply.github.com>
2021-12-11 10:07:07 +0700

    Push code to repo

---
+++ NooberCong.DynamicInclude/Exceptions/InvalidIncludeExpressionException.cs
@@ -0,0 +1,17 @@
+﻿namespace NooberCong.DynamicInclude.Exceptions
+{
+    public class InvalidIncludeExpressionException : Exception
+    {
+        public InvalidIncludeExpressionException(Type declarerType, string propName) : base($"{propName} is not an includable property of {declarerType.Name}")
+        {
+        }
+    }
+
+    public class InvalidIncludeExpressionException<T> : InvalidIncludeExpressionException where T : class
+    {
+        public InvalidIncludeExpressionException(string propName) : base(typeof(T), propName)
+        {
+            
+        }
+    }
+}
